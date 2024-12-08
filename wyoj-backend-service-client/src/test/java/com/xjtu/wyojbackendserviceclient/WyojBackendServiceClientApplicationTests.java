02bb0663fbaf23add9f1f9e00c2ddc46c1b745d9
wy <824745026@qq.com>
2024-12-08 21:04:23 +0800

    OJ微服务3.6

---
+++ wyoj-backend-service-client/src/test/java/com/xjtu/wyojbackendserviceclient/WyojBackendServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjtu.wyojbackendserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyojBackendServiceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
