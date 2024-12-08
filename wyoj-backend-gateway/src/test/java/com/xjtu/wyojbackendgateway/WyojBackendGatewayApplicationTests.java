26b4b7e327a82f10bf34e85ca3ab8e2e9fcbed4d
wy <824745026@qq.com>
2024-12-08 20:59:21 +0800

    OJ微服务3.2

---
+++ wyoj-backend-gateway/src/test/java/com/xjtu/wyojbackendgateway/WyojBackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjtu.wyojbackendgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WyojBackendGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
